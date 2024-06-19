57ddc98352650d6d9621b4aab42137838b9c7ed1
Supawich <supawichwork@gmail.com>
2024-06-19 17:11:44 +0700

    debug2

--- local.sh
+++ local.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git commit -am 'debug2'; git push
+git commit -am 'debug2'
+git push
 
-ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml up -d" &
-ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml up -d" &
-ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml up -d" &
+ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml up -d --build" &
+ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml up -d --build" &
+ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml up -d --build" &
