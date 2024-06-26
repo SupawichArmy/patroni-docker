44c4184ce54c6d0834a395766a38415ae65cf7a7
Supawich <supawichwork@gmail.com>
2024-06-26 16:46:29 +0700

    set   failover_priority node 1 & 2 & 3

--- local.sh
+++ local.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-# git commit -am 'debug2'
-# git push
+# ------------ BUILD IMAGES ---------------------
+# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni01 ." &
+# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni02 ." &
+# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni03 ." &
 
-# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down; docker compose -f docker-compose-node1.yml up -d --build" &
-# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down; docker compose -f docker-compose-node2.yml up -d --build" &
-# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down; docker compose -f docker-compose-node3.yml up -d --build" &
+# ------------ BUILD DOCKER COMPOSE UP ----------
+ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down; docker compose -f docker-compose-node1.yml up -d --build" &
+ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down; docker compose -f docker-compose-node2.yml up -d --build" &
+ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down; docker compose -f docker-compose-node3.yml up -d --build" &
 
-# ------------ BUILD IMAGES ------------
-ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni01 ." &
-ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni02 ." &
-ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni03 ." &
-
-# ------------ BUILD DOCKER COMPOSE ------------
-ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down;" &
-ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down;" &
-ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down;" &
-
-# git commit -am 'debug2'
-# git push
-# scp ./docker-compose-node1.yml adminn01@10.1.3.19:~/patroni-docker
-# scp ./docker-compose-node2.yml adminn02@10.1.3.22:~/patroni-docker
-# scp ./docker-compose-node3.yml adminn03@10.1.3.24:~/patroni-docker
-
-# scp ./docker-compose-node1.yml adminn01@10.1.3.19:~/patroni-docker; ssh adminn01@10.1.3.19 "cd ~/patroni-docker; docker compose -f docker-compose-node1.yml up -d --build" &
-# scp ./docker-compose-node2.yml adminn02@10.1.3.22:~/patroni-docker; ssh adminn02@10.1.3.22 "cd ~/patroni-docker; docker compose -f docker-compose-node2.yml up -d --build" &
-# scp ./docker-compose-node3.yml adminn03@10.1.3.24:~/patroni-docker; ssh adminn03@10.1.3.24 "cd ~/patroni-docker; docker compose -f docker-compose-node3.yml up -d --build" &
+# ------------ DOCKER COMPOSE DOWN --------------
+# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down;" &
+# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down;" &
+# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down;" &
