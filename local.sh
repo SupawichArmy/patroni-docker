f1a750b655e7be489a32288cf3f4e79e272c6400
Supawich <supawichwork@gmail.com>
2024-06-26 10:57:40 +0700

    update

--- local.sh
+++ local.sh
@@ -3,11 +3,9 @@
 # git commit -am 'debug2'
 # git push
 
-# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml up -d --build" &
-# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml up -d --build" &
-# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml up -d --build" &
-
-
+ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml up -d --build" &
+ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml up -d --build" &
+ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml up -d --build" &
 
 # git commit -am 'debug2'
 # git push
@@ -15,6 +13,6 @@
 # scp ./docker-compose-node2.yml adminn02@10.1.3.22:~/patroni-docker
 # scp ./docker-compose-node3.yml adminn03@10.1.3.24:~/patroni-docker
 
-scp ./docker-compose-node1.yml adminn01@10.1.3.19:~/patroni-docker; ssh adminn01@10.1.3.19 "cd ~/patroni-docker; docker compose -f docker-compose-node1.yml up -d --build" &
-scp ./docker-compose-node2.yml adminn02@10.1.3.22:~/patroni-docker; ssh adminn02@10.1.3.22 "cd ~/patroni-docker; docker compose -f docker-compose-node2.yml up -d --build" &
-scp ./docker-compose-node3.yml adminn03@10.1.3.24:~/patroni-docker; ssh adminn03@10.1.3.24 "cd ~/patroni-docker; docker compose -f docker-compose-node3.yml up -d --build" &
+# scp ./docker-compose-node1.yml adminn01@10.1.3.19:~/patroni-docker; ssh adminn01@10.1.3.19 "cd ~/patroni-docker; docker compose -f docker-compose-node1.yml up -d --build" &
+# scp ./docker-compose-node2.yml adminn02@10.1.3.22:~/patroni-docker; ssh adminn02@10.1.3.22 "cd ~/patroni-docker; docker compose -f docker-compose-node2.yml up -d --build" &
+# scp ./docker-compose-node3.yml adminn03@10.1.3.24:~/patroni-docker; ssh adminn03@10.1.3.24 "cd ~/patroni-docker; docker compose -f docker-compose-node3.yml up -d --build" &
