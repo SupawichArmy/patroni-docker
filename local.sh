be9e96ffbc3a508d0f53205eb4dcf5ec06fcb640
Supawich <supawichwork@gmail.com>
2024-06-26 11:16:21 +0700

    update

--- local.sh
+++ local.sh
@@ -3,9 +3,13 @@
 # git commit -am 'debug2'
 # git push
 
-ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down; docker compose -f docker-compose-node1.yml up -d --build" &
-ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down; docker compose -f docker-compose-node2.yml up -d --build" &
-ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down; docker compose -f docker-compose-node3.yml up -d --build" &
+# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down; docker compose -f docker-compose-node1.yml up -d --build" &
+# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down; docker compose -f docker-compose-node2.yml up -d --build" &
+# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down; docker compose -f docker-compose-node3.yml up -d --build" &
+
+ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down;" &
+ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down;" &
+ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down;" &
 
 # git commit -am 'debug2'
 # git push
