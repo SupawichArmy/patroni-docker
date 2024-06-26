46589fbc512e0676009ac568f26262d45ce3dcdf
Supawich <supawichwork@gmail.com>
2024-06-26 16:17:00 +0700

    Demo Node 1 & 2 & 3

--- local.sh
+++ local.sh
@@ -7,6 +7,12 @@
 # ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down; docker compose -f docker-compose-node2.yml up -d --build" &
 # ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down; docker compose -f docker-compose-node3.yml up -d --build" &
 
+# ------------ BUILD IMAGES ------------
+ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni01 ." &
+ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni02 ." &
+ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni03 ." &
+
+# ------------ BUILD DOCKER COMPOSE ------------
 ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node1.yml down;" &
 ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node2.yml down;" &
 ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker compose -f docker-compose-node3.yml down;" &
