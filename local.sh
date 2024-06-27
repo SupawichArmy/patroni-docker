d34e23f68060ed9a9cde2de9c152cf887ebfe5d0
Supawich <supawichwork@gmail.com>
2024-06-27 17:42:40 +0700

    Add Dockerfile 1 & 2 & 3

---
+++ local.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ------------ BUILD IMAGES ---------------------
+# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni01 ." &
+# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni02 ." &
+# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni03 ." &
+
+------------ BUILD DOCKER COMPOSE UP ----------
+ssh adminn01@10.1.3.19 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni01 -f Dockerfile-node1 .; docker compose -f docker-compose-node1.yml down; docker compose -f docker-compose-node1.yml up -d --build " &
+ssh adminn02@10.1.3.22 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni02 -f Dockerfile-node2 .; docker compose -f docker-compose-node2.yml down; docker compose -f docker-compose-node2.yml up -d --build " &
+ssh adminn03@10.1.3.24 "cd ~/patroni-docker; git fetch ; git pull; docker build -t patroni03 -f Dockerfile-node3 .; docker compose -f docker-compose-node3.yml down; docker compose -f docker-compose-node3.yml up -d --build " &
+
+
+# # ------------ BUILD DOCKER COMPOSE DOWN ----------
+# ssh adminn01@10.1.3.19 "cd ~/patroni-docker; docker compose -f docker-compose-node1.yml down; " &
+# ssh adminn02@10.1.3.22 "cd ~/patroni-docker; docker compose -f docker-compose-node2.yml down; " &
+# ssh adminn03@10.1.3.24 "cd ~/patroni-docker; docker compose -f docker-compose-node3.yml down; " &
